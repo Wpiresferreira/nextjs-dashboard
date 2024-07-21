13235fd25801e0e6fb8af8196534cd58b0da55b8
Wagner Pires Ferreira <wagner_pires@icloud.com>
2024-07-20 21:22:19 -0600

    Chapter 7

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -8,6 +8,8 @@ export default async function LatestInvoices({
 }: {
   latestInvoices: LatestInvoice[];
 }) {
+
+  const newDate = new Date(Date.now());
   return (
     <div className="flex w-full flex-col md:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -16,7 +18,7 @@ export default async function LatestInvoices({
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
         {/* NOTE: Uncomment this code in Chapter 7 */}
 
-        {/* <div className="bg-white px-6">
+        <div className="bg-white px-6">
           {latestInvoices.map((invoice, i) => {
             return (
               <div
@@ -53,10 +55,10 @@ export default async function LatestInvoices({
               </div>
             );
           })}
-        </div> */}
+        </div>
         <div className="flex items-center pb-2 pt-6">
           <ArrowPathIcon className="h-5 w-5 text-gray-500" />
-          <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
+          <h3 className="ml-2 text-sm text-gray-500 ">Updated {newDate.toLocaleString()}</h3>
         </div>
       </div>
     </div>
